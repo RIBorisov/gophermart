9c5590effc6d7619196186b139d709f210b96a97
RIBorisov <borissoffr@yandex.ru>
2024-07-13 14:13:53 +0300

    minor: remove default flag values

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -8,9 +8,9 @@ var f Service
 
 func parseFlags() *Service {
 	if !flag.Parsed() {
-		flag.StringVar(&f.RunAddress, "a", "localhost:8089", "Host:port where server running")
+		flag.StringVar(&f.RunAddress, "a", "", "Host:port where server running")
 		flag.StringVar(&f.DatabaseDSN, "d", "", "Database DSN")
-		flag.StringVar(&f.AccrualSystemAddress, "r", ":8080", "Accrual system address")
+		flag.StringVar(&f.AccrualSystemAddress, "r", "", "Accrual system address")
 		flag.Parse()
 	}
 	return &f
